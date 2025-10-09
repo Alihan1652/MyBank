fd63904dbc084c64416cfede6ef9e80bf4a3612f
Alihan1652 <alixan88as@gmail.com>
2025-10-10 00:28:16 +0600

    Обнова

---
+++ app/src/main/java/com/example/mybank/data/model/network/ApiClient.kt
@@ -0,0 +1,24 @@
+package com.example.mybank.data.model.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://68e7e57a10e3f82fbf4139d7.mockapi.io/")
+        .client(httpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val accountsApi: AccountsApi = retrofit.create(AccountsApi::class.java)
+}
--- app/src/main/java/com/example/mybank/ui/MainActivity.kt
+++ app/src/main/java/com/example/mybank/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mybank.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mybank.data.model.Account
 import com.example.mybank.databinding.ActivityMainBinding
+import com.example.mybank.databinding.DialogAddBinding
 import com.example.mybank.domain.presenter.AccountContracts
 import com.example.mybank.domain.presenter.AccountPresenter
 import com.example.mybank.ui.adapter.AcoountsAdapter
@@ -22,6 +25,28 @@ class MainActivity : AppCompatActivity(), AccountContracts.View {
         setContentView(binding.root)
         initAdapter()
         presenter = AccountPresenter(this)
+
+        binding.bthAdd.setOnClickListener {
+            showAddDialog()
+        }
+    }
+
+    private fun showAddDialog(){
+        val binding = DialogAddBinding.inflate(LayoutInflater.from(this))
+        with(binding){
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("Добовление нового счета")
+                .setView(binding.root)
+                .setPositiveButton("Добавить") {_,_ ->
+                    val account = Account(
+                        name = etName.text.toString(),
+                        balance = etBalance.text.toString().toInt(),
+                        currency = etCurrency.text.toString()
+                    )
+                    presenter.addAccount(account)
+                }
+                .show()
+        }
     }
 
     override fun onResume() {
